c31b1a179f86f2d94c4f9b615370c4bf86784b5a
damhack <damhack@me.com>
2020-11-14 23:22:31 +0000

    Create mjpeg_camera.py
    Remote MJPEG IP camera

---
+++ jetcam/mjpeg_camera.py
@@ -0,0 +1,42 @@
+from .camera import Camera
+import atexit
+import cv2
+import numpy as np
+import threading
+import traitlets
+
+
+class MJPEGCamera(Camera):
+    
+    capture_device = traitlets.Integer(default_value=0)
+    capture_fps = traitlets.Integer(default_value=30)
+    capture_width = traitlets.Integer(default_value=640)
+    capture_height = traitlets.Integer(default_value=480)
+    capture_location = traitlets.String(default_value="127.0.0.1:8080/stream")
+    
+    def __init__(self, *args, **kwargs):
+        super(MJPEGCamera, self).__init__(*args, **kwargs)
+        try:
+            self.cap = cv2.VideoCapture(self._mjpeg_pipeline(self.camera_location), cv2.CAP_GSTREAMER)
+
+            re, image = self.cap.read()
+
+            if not re:
+                raise RuntimeError('Could not read image from camera.')
+        except:
+            raise RuntimeError(
+                'Could not initialize camera.  Please see error trace.')
+
+        atexit.register(self.cap.release)
+                
+    def _gst_str(self):
+        return 'souphttpsrc location=%s do-timestamp=true is_live=true ! multipartdemux ! jpegdec ! videorate ! videoscale ! video/x-raw, width=(int)%d, height=(int)%d, framerate=(fraction)%d/1 ! videoconvert ! video/x-raw, format=BGR ! appsink' % (
+                "http://" + self.capture_location, self.capture_width, self.capture_height, self.capture_fps, self.width, self.height)
+
+    
+    def _read(self):
+        re, image = self.cap.read()
+        if re:
+            return image
+        else:
+            raise RuntimeError('Could not read image from camera')
